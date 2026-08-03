dbf57077d9f4598e719ed2d91da0b5d92b8d8531
sujith <sujithsumo@gmail.com>
2026-08-03 12:04:51 +0530

    Update 02-install-cloudwatch-agent.sh

--- Scripts/02-install-cloudwatch-agent.sh
+++ Scripts/02-install-cloudwatch-agent.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 sudo yum install amazon-cloudwatch-agent -y
+amazon-cloudwatch-agent-ctl -a status
+sudo mkdir -p /opt/aws/amazon-cloudwatch-agent/etc
+sudo nano /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+-a fetch-config \
+-m ec2 \
+-c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json \
+-s
+sudo systemctl status amazon-cloudwatch-agent
+cat /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
